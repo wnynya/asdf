b47404acfda49beb0e48bcd95eee7870284b6f1a
Wany <sung@wanyne.com>
2024-04-29 00:07:15 +0900

    up

--- src/public/index.mjs
+++ src/public/index.mjs
@@ -84,6 +84,60 @@ class Client {
   async getHistory() {}
 
   destroy() {}
+
+  MENU = {
+    getCategoryElement(menu) {
+      const element = document.createElement('div');
+      element.classList.add('category');
+      element.setAttribute('category', menu.category);
+      let html = ``;
+      html += `<div class="title">${menu.category}</div>`;
+      html += `<div class="content">`;
+      html += `</div>`;
+      element.innerHTML = html;
+      return element;
+    },
+
+    getElement(menu) {
+      const element = document.createElement('div');
+      element.classList.add('menu');
+      element.setAttribute('menu', menu.id);
+      let html = ``;
+      html += `<div class="wrapper">`;
+      if (menu.image) {
+        html += `  <div class="image">`;
+        html += `    <img src="${menu.image}" />`;
+        html += `  </div>`;
+      }
+      html += `  <div class="text">`;
+      html += `    <div class="desc">`;
+      html += `      <div class="title">`;
+      html += `        <span class="text">${menu.name}</span>`;
+      html += `        <div class="buttons hide">`;
+      html += `          <button class="remove"><span class="material-symbols-outlined"> remove </span></button>`;
+      html += `          <div class="count">0</div>`;
+      html += `          <button class="add"><span class="material-symbols-outlined"> add </span></button>`;
+      html += `        </div>`;
+      html += `      </div>`;
+      html += `      <div class="content">${menu.desc}</div>`;
+      html += `    </div>`;
+      html += `    <div class="price">${menu.price.toLocaleString(
+        'ko-KR'
+      )} 원</div>`;
+      html += `  </div>`;
+      html += `</div>`;
+      element.innerHTML = html;
+      return element;
+    },
+  };
+
+  CART = {
+    addMenu(menu) {},
+
+    removeMenu(menu) {},
+
+    order() {},
+  };
 }
 
 class UI {
@@ -104,7 +158,7 @@ class UI {
             break;
           }
           case 'order': {
-            this.panelBottomClose();
+            this.PANEL_BOTTOM$close();
             break;
           }
           default: {
@@ -117,23 +171,26 @@ class UI {
         const phase = this.element.panel.bottom.getAttribute('phase');
         switch (phase) {
           case 'cart': {
-            this.panelBottomOrder();
+            this.PANEL_BOTTOM$openOrder();
             break;
           }
           case 'order': {
             break;
           }
           default: {
-            this.panelBottomCart();
+            this.PANEL_BOTTOM - openCart();
             break;
           }
         }
       },
       'panel-bottom-cart-close': (event) => {
-        this.panelBottomClose();
+        this.PANEL_BOTTOM$close();
       },
       'panel-bottom-order-close': (event) => {
-        this.panelBottomClose();
+        this.PANEL_BOTTOM$close();
+      },
+      'panel-bottom-close': (event) => {
+        this.PANEL_BOTTOM$close();
       },
     };
 
@@ -143,9 +200,9 @@ class UI {
       const buttonName = buttonID.toUpperCase().replace(/\-/g, '_');
       this.button[buttonName] = buttonElement;
     }
-  }
 
-  scroll;
+    this.PANEL_BOTTOM = {};
+  }
 
   displayMenu() {
     document.querySelector('#menu').innerHTML = '';
@@ -157,7 +214,47 @@ class UI {
         const categoryElement = createCategoryElement(menu.category);
         categories[menu.category] = categoryElement;
       }
-      const menuElement = createMenuElement(menu);
+      const menuElement = this.createMenuElement(menu);
+      menuElement.addEventListener('click', (event) => {
+        let target = event.target;
+        let mode = 'normal';
+        while (!target.classList.contains('menu')) {
+          if (target.classList.contains('remove')) {
+            mode = 'remove';
+          }
+          if (target.classList.contains('add')) {
+            mode = 'add';
+          }
+          target = target.parentElement;
+        }
+
+        const menuID = target.getAttribute('menu');
+        console.log(_this);
+        let menu = _this.client.menu[menuID];
+
+        if (mode == 'normal' && !_this.client.cart[menu.id]) {
+          blink();
+          this.addMenu(menu);
+        } else if (mode == 'add') {
+          blink();
+          this.addMenu(menu);
+        } else if (mode == 'remove') {
+          blink();
+          this.removeMenu(menu);
+        }
+
+        function blink() {
+          target.style.background = 'rgba(255, 100, 60, 0.5)';
+          setTimeout(() => {
+            target.style.background = 'rgba(255, 100, 60, 0)';
+            target.style.transition = 'background 0.4s ease-out';
+          }, 100);
+          setTimeout(() => {
+            target.style.background = null;
+            target.style.transition = null;
+          }, 500);
+        }
+      });
       categories[menu.category]
         .querySelector('.content')
         .appendChild(menuElement);
@@ -194,7 +291,7 @@ class UI {
       html += `    <div class="desc">`;
       html += `      <div class="title">`;
       html += `        <span class="text">${data.name}</span>`;
-      html += `        <div class="buttons">`;
+      html += `        <div class="buttons hide">`;
       html += `          <button class="remove"><span class="material-symbols-outlined">remove</span></button>`;
       html += `          <div class="count">0</div>`;
       html += `          <button class="add"><span class="material-symbols-outlined">add</span></button>`;
@@ -210,18 +307,43 @@ class UI {
       element.innerHTML = html;
       element.addEventListener('click', (event) => {
         let target = event.target;
+        let mode = 'normal';
         while (!target.classList.contains('menu')) {
+          if (target.classList.contains('remove')) {
+            mode = 'remove';
+          }
+          if (target.classList.contains('add')) {
+            mode = 'add';
+          }
           target = target.parentElement;
         }
-        target.style.background = 'rgba(255, 100, 60, 0.5)';
-        setTimeout(() => {
-          target.style.background = 'rgba(255, 100, 60, 0)';
-          target.style.transition = 'background 0.4s ease-out';
-        }, 100);
-        setTimeout(() => {
-          target.style.background = null;
-          target.style.transition = null;
-        }, 500);
+
+        const menuID = target.getAttribute('menu');
+        console.log(_this);
+        let menu = _this.client.menu[menuID];
+
+        if (mode == 'normal' && !_this.client.cart[menu.id]) {
+          blink();
+          _this.addMenu(menu);
+        } else if (mode == 'add') {
+          blink();
+          _this.addMenu(menu);
+        } else if (mode == 'remove') {
+          blink();
+          _this.removeMenu(menu);
+        }
+
+        function blink() {
+          target.style.background = 'rgba(255, 100, 60, 0.5)';
+          setTimeout(() => {
+            target.style.background = 'rgba(255, 100, 60, 0)';
+            target.style.transition = 'background 0.4s ease-out';
+          }, 100);
+          setTimeout(() => {
+            target.style.background = null;
+            target.style.transition = null;
+          }, 500);
+        }
         /*
         const rect = target.getBoundingClientRect();
         const temp = document.createElement('div');
@@ -264,48 +386,222 @@ class UI {
     }, 1000);
   }
 
-  panelBottomCart() {
-    this.element.panel.bottom.setAttribute('phase', 'cart');
-    this.button.PANEL_BOTTOM_RIGHT.querySelector('.text').innerText =
-      '주문하기';
-    this.button.PANEL_BOTTOM_LEFT.disabled = true;
-    this.button.PANEL_BOTTOM_RIGHT.disabled = true;
-    setTimeout(() => {
-      this.button.PANEL_BOTTOM_RIGHT.disabled = false;
-    }, 1000);
-    this.panelBottomUpdate(0);
-  }
-
-  panelBottomOrder() {
-    this.element.panel.bottom.setAttribute('phase', 'order');
-    this.button.PANEL_BOTTOM_LEFT.querySelector('.text').innerText = '닫기';
-    this.button.PANEL_BOTTOM_LEFT.disabled = true;
-    setTimeout(() => {
-      this.button.PANEL_BOTTOM_LEFT.disabled = false;
-    }, 1000);
-    this.panelBottomUpdate(0);
-  }
-
-  panelBottomClose() {
-    this.element.panel.bottom.setAttribute('phase', '');
-    this.button.PANEL_BOTTOM_LEFT.querySelector('.text').innerText =
-      '직원 호출';
-    this.button.PANEL_BOTTOM_RIGHT.querySelector('.text').innerText =
-      '담은 메뉴 보기';
-    this.button.PANEL_BOTTOM_LEFT.disabled = false;
-    this.button.PANEL_BOTTOM_RIGHT.disabled = false;
-    this.panelBottomUpdate(10);
-  }
-
-  panelBottomUpdate(n) {
-    const count = this.button.PANEL_BOTTOM_RIGHT.querySelector('.count');
-    count.innerText = `메뉴 ${n}개 담음`;
-    if (n <= 0) {
-      count.setAttribute('phase', 'hide');
-    } else {
-      count.setAttribute('phase', 'show');
+  addMenu(menu) {
+    if (!this.client.cart[menu.id]) {
+      this.client.cart[menu.id] = 0;
     }
+
+    const menuElement = document.querySelector(`[menu="${menu.id}"]`);
+    this.client.cart[menu.id] += 1;
+    menuElement.querySelector('.buttons > .count').innerHTML =
+      this.client.cart[menu.id];
+    menuElement.querySelector('.buttons').classList.remove('hide');
+
+    this.PANEL_BOTTOM$updateLabel();
   }
+
+  removeMenu(menu) {
+    if (!this.client.cart[menu.id]) {
+      return;
+    }
+
+    const menuElement = document.querySelector(`[menu="${menu.id}"]`);
+    this.client.cart[menu.id] -= 1;
+    menuElement.querySelector('.buttons > .count').innerHTML =
+      this.client.cart[menu.id];
+
+    if (this.client.cart[menu.id] <= 0) {
+      delete this.client.cart[menu.id];
+      menuElement.querySelector('.buttons').classList.add('hide');
+    }
+
+    this.PANEL_BOTTOM$updateLabel();
+  }
+
+  MAIN = {
+    displayBooth() {},
+
+    displayMenu() {
+      let categories = {};
+
+      for (const [menu, menuID] in Object.entries(this.client.menu)) {
+        if (!categories[menu.category]) {
+          const categoryElement = this.client.MENU.getCategoryElement(menu);
+          categories[menu.category] = categoryElement;
+        }
+
+        const menuElement = this.client.MENU.getElement(menu);
+        menuElement.addEventListener('click', (event) => {
+          let target = event.target;
+          let mode = 'normal';
+          while (!target.classList.contains('menu')) {
+            if (target.classList.contains('remove')) {
+              mode = 'remove';
+            }
+            if (target.classList.contains('add')) {
+              mode = 'add';
+            }
+            target = target.parentElement;
+          }
+
+          const menuID = target.getAttribute('menu');
+          const menu = this.client.menu[menuID];
+
+          if (mode == 'normal' && !this.client.cart[menu.id]) {
+            blink();
+            this.addMenu(menu);
+          } else if (mode == 'add') {
+            blink();
+            this.addMenu(menu);
+          } else if (mode == 'remove') {
+            blink();
+            this.removeMenu(menu);
+          }
+
+          function blink() {
+            target.style.background = 'rgba(255, 100, 60, 0.5)';
+            setTimeout(() => {
+              target.style.background = 'rgba(255, 100, 60, 0)';
+              target.style.transition = 'background 0.4s ease-out';
+            }, 100);
+            setTimeout(() => {
+              target.style.background = null;
+              target.style.transition = null;
+            }, 500);
+          }
+        });
+
+        categories[menu.category]
+          .querySelector('.content')
+          .appendChild(menuElement);
+      }
+
+      document.querySelector('#menu').innerHTML = '';
+      for (const category in categories) {
+        document.querySelector('#menu').appendChild(categories[category]);
+      }
+    },
+
+    updateMenu() {},
+
+    createCategoryElement(data) {
+      const element = document.createElement('div');
+      element.classList.add('category');
+      element.setAttribute('category', data);
+      let html = ``;
+      html += `<div class="title">${data}</div>`;
+      html += `<div class="content">`;
+      html += `</div>`;
+      element.innerHTML = html;
+      return element;
+    },
+
+    createMenuElement() {
+      const element = document.createElement('div');
+      element.classList.add('menu');
+      element.setAttribute('menu', data.id);
+      let html = ``;
+      html += `<div class="wrapper">`;
+      if (data.image) {
+        html += `  <div class="image">`;
+        html += `    <img src="${data.image}" />`;
+        html += `  </div>`;
+      }
+      html += `  <div class="text">`;
+      html += `    <div class="desc">`;
+      html += `      <div class="title">`;
+      html += `        <span class="text">${data.name}</span>`;
+      html += `        <div class="buttons hide">`;
+      html += `          <button class="remove"><span class="material-symbols-outlined"> remove </span></button>`;
+      html += `          <div class="count">0</div>`;
+      html += `          <button class="add"><span class="material-symbols-outlined"> add </span></button>`;
+      html += `        </div>`;
+      html += `      </div>`;
+      html += `      <div class="content">${data.desc}</div>`;
+      html += `    </div>`;
+      html += `    <div class="price">${data.price.toLocaleString(
+        'ko-KR'
+      )} 원</div>`;
+      html += `  </div>`;
+      html += `</div>`;
+      element.innerHTML = html;
+      return element;
+    },
+  };
+
+  PANEL_MENU = {
+    open(menu) {},
+
+    close() {},
+  };
+
+  PANEL_CART = {
+    open() {
+      this.element.panel.bottom.setAttribute('phase', 'cart');
+      this.button.PANEL_BOTTOM_RIGHT.querySelector('.text').innerText =
+        '주문하기';
+      this.button.PANEL_BOTTOM_LEFT.disabled = true;
+      this.button.PANEL_BOTTOM_RIGHT.disabled = true;
+      setTimeout(() => {
+        this.button.PANEL_BOTTOM_RIGHT.disabled = false;
+      }, 1000);
+      this.PANEL_BOTTOM$updateLabel(true);
+    },
+
+    displayMenu() {},
+
+    updateMenu() {},
+
+    order() {
+      this.element.panel.bottom.setAttribute('phase', 'order');
+      this.button.PANEL_BOTTOM_LEFT.querySelector('.text').innerText = '닫기';
+      this.button.PANEL_BOTTOM_LEFT.disabled = true;
+      setTimeout(() => {
+        this.button.PANEL_BOTTOM_LEFT.disabled = false;
+      }, 1000);
+      this.PANEL_BOTTOM$updateLabel(true);
+    },
+
+    close() {
+      this.element.panel.bottom.setAttribute('phase', '');
+      this.button.PANEL_BOTTOM_LEFT.querySelector('.text').innerText =
+        '직원 호출';
+      this.button.PANEL_BOTTOM_RIGHT.querySelector('.text').innerText =
+        '담은 메뉴 보기';
+      this.button.PANEL_BOTTOM_LEFT.disabled = false;
+      this.button.PANEL_BOTTOM_RIGHT.disabled = false;
+      this.PANEL_BOTTOM$updateLabel();
+    },
+
+    updateLabel(hide = false) {
+      let total = 0;
+      if (!hide) {
+        for (const menu in this.client.cart) {
+          total += this.client.cart[menu];
+        }
+      }
+
+      const count = this.button.PANEL_BOTTOM_RIGHT.querySelector('.count');
+      count.innerText = `메뉴 ${total}개 담음`;
+      if (total <= 0) {
+        count.setAttribute('phase', 'hide');
+      } else {
+        count.setAttribute('phase', 'show');
+      }
+    },
+  };
+
+  PANEL_HISTORY = {
+    open() {},
+
+    close() {},
+  };
+
+  PANEL_OPTIONS = {
+    open() {},
+
+    close() {},
+  };
 
   openHistory() {}
 
